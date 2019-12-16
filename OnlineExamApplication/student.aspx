8a76067f8bfc78c5a2e0aafad90f3baa7b49a6e2
rabbsmaths <rabbimathake@gmail.com>
2019-12-16 13:44:04 +0200

    added lookup to read available test

--- OnlineExamApplication/student.aspx
+++ OnlineExamApplication/student.aspx
@@ -1,3 +1,25 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="student.aspx.cs" Inherits="OnlineExamApplication.student" %>
+﻿<%@ Page Title="Write Exam" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="student.aspx.cs" Inherits="OnlineExamApplication.student" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+     <h2><%: Title %>.</h2>
+    <p class="text-danger">
+        <asp:Literal runat="server" ID="ErrorMessage" />
+    </p>
+
+    <div class="form-horizontal">
+        <hr />
+        <div id="divSelectTest" runat ="server">
+          <div class="form-group">
+            <asp:Label runat="server" AssociatedControlID="dlSelectTest" CssClass="col-md-2 control-label">Select Exam</asp:Label>
+            <div class="col-md-10">
+                 <asp:DropDownList CssClass="dropdopwn-menu form-control" ID="dlSelectTest" runat="server" Width="281px" AppendDataBoundItems="False" />
+            </div>
+        </div>
+            <div class="form-group">
+            <div class="col-md-offset-2 col-md-10">
+                <asp:Button runat="server"  Text="Start Exam" CssClass="btn btn-default" ID="btnStartExam" OnClick="btnStartExam_Click"  />
+            </div>
+        </div>
+       </div>
+
+    </div>
 </asp:Content>
--- OnlineExamApplication/student.aspx.cs
+++ OnlineExamApplication/student.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Windows.Forms;
 
 namespace OnlineExamApplication
 {
@@ -11,7 +15,73 @@ namespace OnlineExamApplication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //connecting string 
+            string connString = ConfigurationManager.ConnectionStrings["onlineExamDB"].ConnectionString;
+
+            //connection
+            SqlConnection con = new SqlConnection(connString);
+
+            //open connection
+            con.Open();
+
+            if (!Page.IsPostBack)
+            {
+               
+                bindLookUpData(connString); //load data
+            }
 
         }
+
+
+        //bind the lookup
+        public void bindLookUpData(string connString)
+        {
+
+            //connection
+            SqlConnection con = new SqlConnection(connString);
+
+            //open connection
+            con.Open();
+            //bind the questions
+            SqlCommand command = new SqlCommand("SELECT t_TestDescription,t_testNo FROM tblTest t, tblUser u WHERE u.ID = t.user_ID ", con);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dlSelectTest.DataSource = dt;
+            dlSelectTest.DataTextField = "t_TestDescription";
+            dlSelectTest.DataValueField = "t_testNo";
+            dlSelectTest.DataBind();
+            //check if any exam questions available for this user
+            SqlDataReader rd = command.ExecuteReader();
+            if (rd.Read())
+            {
+               
+            }
+            else
+            {
+                
+            }
+            con.Close();
+        }
+
+        protected void btnStartExam_Click(object sender, EventArgs e)
+        {
+            string connString = ConfigurationManager.ConnectionStrings["onlineExamDB"].ConnectionString;
+
+            //connection
+            SqlConnection con = new SqlConnection(connString);
+
+            //open connection
+            con.Open();
+
+            //bind the questions int gridview
+            SqlCommand command = new SqlCommand("SELECT t.t_TestDescription, q.Question, q.Question_Type, q.Answer FROM tblTest t, tblQuestion q WHERE t.t_testNo = q.t_testNo AND t.t_testNo = @testNo Order By  t.t_TestDescription", con);
+            command.Parameters.AddWithValue("@testNo", dlSelectTest.SelectedValue.ToString());
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+ 
+            con.Close();//close connection
+        }
+
     }
 }
